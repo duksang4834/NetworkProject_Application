321b5c2d3515884fa739b8a629a995b2bcc684b4
Duck <deoksang@Duckui-MacBook-Pro.local>
2016-05-11 22:37:18 +0900

    temp

--- www/js/controllers/main/main-list-controller.js
+++ www/js/controllers/main/main-list-controller.js
@@ -1,21 +1,37 @@
 angular.module('bookApp.controllers')
 
-	.controller('MainListCtrl', function($scope, $state, $ionicScrollDelegate, $cordovaDevice) {
+	.controller('MainListCtrl', function($scope, $state, $ionicSideMenuDelegate, $cordovaDevice, $http) {
+		
+		document.addEventListener('deviceReady', function () {
+			var uuid = $cordovaDevice.getUUID();
 
-  	$scope.goRentSearch = function () {
+			$http({
+				method: 'POST',
+				url: 'http://ec2-52-79-167-53.ap-northeast-2.compute.amazonaws.com:8080/registerDevice',
+				params: {
+					account: 'test',
+					device: uuid
+				}
+			});
+		});
 
+  		$scope.goRentSearch = function () {
 			$state.go('rentSearch.Main');
 		};
 
-	$scope.goRegisterSearch = function () {
-
+		$scope.goRegisterSearch = function () {
 			$state.go('registerSearch.Main');
 		};
 
-	$scope.toggleLeft = function() {
+		$scope.toggleLeft = function() {
     		$ionicSideMenuDelegate.toggleLeft();
   		};
 
-		var uuid = $cordovaDevice.getUUID();
+  		$scope.moveScroll = function (e) {
+  			var position = e.detail.scrollTop,
+  			target = document.getElementById('add-button');
+
+  			target.style.top = position + 'px';
+  		};
 
 	});
